2e2784f1afc776fc4e946b5bf1bf78f9768ea34a
Nagmani Kashyap <singh.tus.0001@gmjail.com>
2021-07-25 15:41:38 +0530

    Hello

---
+++ ig.js
@@ -0,0 +1,11 @@
+console.log("Hello JS :) ");
+let a = 0;
+a = "Hello JavaScript";
+a = 'I AM A HUMAN';
+let b = 'I AM A BOY';
+console.log(a);
+console.log(b);
+let c = 10;
+for(let i = 0; i <= c; i++){
+console.log("THIS IS MY CODE " + i);
+}
